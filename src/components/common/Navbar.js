42368aaa814fc13edc414d05662f63cca09832b2
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2017-12-26 01:25:09 +0700

    style navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import logo from './../../img/logo.jpg';
+
 
 const Navbar = () => {
     return (
-        <div className="row">
-            <div className="col-md-12 text-center navBar">Navbar</div>
-        </div>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light">
+            <a className="navbar-brand" href="#">
+                <img src={logo} width="40px;"/>
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">หน้าแรก
+                            <span className="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">ผลิตภัณฑ์</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">คุณสมบัติผู้สมัคร</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">เกี่ยวกับเรา</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
     );
 };
 
