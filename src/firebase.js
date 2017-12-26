cf66d03aa6a2aa0c867d87ba36d4a9bcbca4ef8a
Ratchasak Ranron <ratchasak.ranron@gmail.com>
2017-12-26 09:48:26 +0700

    add firebase deployment script

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 
 export const init = () => {
-    var config = {
+    var configDev = {
         apiKey: "AIzaSyCK4qBLbIUGCXf937tMgnzWy5kr_Bgqmwg",
         authDomain: "moneydi-f61bd.firebaseapp.com",
         databaseURL: "https://moneydi-f61bd.firebaseio.com",
@@ -9,5 +9,13 @@ export const init = () => {
         storageBucket: "moneydi-f61bd.appspot.com",
         messagingSenderId: "710399914064"
     };
-    firebase.initializeApp(config);
+    var configProd = {
+        apiKey: "AIzaSyChvM_r7NS8ffNCaMEIB8H2gY9HJAfKc-o",
+        authDomain: "moneydi-5556d.firebaseapp.com",
+        databaseURL: "https://moneydi-5556d.firebaseio.com",
+        projectId: "moneydi-5556d",
+        storageBucket: "moneydi-5556d.appspot.com",
+        messagingSenderId: "196389492792"
+    };
+    firebase.initializeApp(configDev);
 };
