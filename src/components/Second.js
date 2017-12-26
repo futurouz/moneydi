cb412480405e74bb0d21f146c07d0202e55040a6
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2017-12-26 12:29:03 +0700

    style first page anchor

--- src/components/Second.js
+++ src/components/Second.js
@@ -5,24 +5,26 @@ import checkHand from './../img/checkHand.png';
 
 const Second = () => {
     return (
-        <div className="secondContainer">
-            <div className="row">
-                <div className="col-md-12 col-xs-12">
-                    <h1>เงินให้ยืม มันนี่ดิ</h1>
+        <div>
+            <div className="secondContainer" id="product">
+                <div className="row">
+                    <div className="col-md-12 col-xs-12">
+                        <h1>เงินให้ยืม มันนี่ดิ</h1>
+                    </div>
                 </div>
-            </div>
-            <div className="row text-center">
-                <div className="col-md-4">
-                    <img src={clock} alt="clock" className="img-responsive"/>
-                    <p>ผู้ที่ต้องการใช้เงินเดือนล่วงหน้า คืนสิ้นเดือน​</p>
-                </div>
-                <div className="col-md-4">
-                    <img src={zero} alt="zero" className="img-responsive"/>
-                    <p>เงินยืม ไม่มีดอกเบี้ย​​</p>
-                </div>
-                < div className="col-md-4">
-                    <img src={checkHand} alt="checkHand" className="img-responsive"/>
-                    <p>สมัครออนไลน์ รู้ผลและได้รับเงินใน 24 ชม​</p>
+                <div className="row text-center">
+                    <div className="col-md-4">
+                        <img src={clock} alt="clock" className="img-responsive"/>
+                        <p>ผู้ที่ต้องการใช้เงินเดือนล่วงหน้า คืนสิ้นเดือน​</p>
+                    </div>
+                    <div className="col-md-4">
+                        <img src={zero} alt="zero" className="img-responsive"/>
+                        <p>เงินยืม ไม่มีดอกเบี้ย​​</p>
+                    </div>
+                    < div className="col-md-4">
+                        <img src={checkHand} alt="checkHand" className="img-responsive"/>
+                        <p>สมัครออนไลน์ รู้ผลและได้รับเงินใน 24 ชม​</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -5,7 +5,7 @@ import logo from './../../img/logo.jpg';
 const Navbar = () => {
     return (
         <nav className="navbar fixed-top navbar-expand-lg navbar-light">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/">
                 <img src={logo} width="40px;"/>
             </a>
             <button
@@ -22,18 +22,18 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">หน้าแรก
+                        <a className="nav-link" href="/">หน้าแรก
                             <span className="sr-only">(current)</span>
                         </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">ผลิตภัณฑ์</a>
+                        <a className="nav-link" href="/#product">ผลิตภัณฑ์</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">คุณสมบัติผู้สมัคร</a>
+                        <a className="nav-link" href="/#qualification">คุณสมบัติผู้สมัคร</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">เกี่ยวกับเรา</a>
+                        <a className="nav-link" href="/#aboutus">เกี่ยวกับเรา</a>
                     </li>
                 </ul>
             </div>
