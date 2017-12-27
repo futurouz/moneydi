e55e4b5de403bd22ce5f1b3409559ff203b85b01
Kunanan <kunanan.tas@mail.kmutt.ac.th>
2017-12-27 12:26:16 +0700

    fixed style home page

--- src/components/home/sections/HomeQualification.js
+++ src/components/home/sections/HomeQualification.js
@@ -4,42 +4,44 @@ import {Link} from 'react-router-dom'
 const HomeQualification = () => {
     return (
         <div className="fourthContainer" id="qualification">
-            <div className="row">
-                <div className="col-md-12">
-                    <h3>คุณสมบัติผู้สมัคร</h3>
+            <div className="content">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h3>คุณสมบัติผู้สมัคร</h3>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <ul>
-                        <li>สัญชาติไทย อายุ 21 ปีขึ้นไป</li>
-                        <li>ทำงานอยู่ในกทม.และปริมณฑล</li>
-                        <li>มีรายได้ต่อเดือนตั้งแต่ 15,000 บาทขึ้นไป และมีอายุงานตั้งแต่ 4 เดือนขึ้นไป</li>
-                        <li>มีเหตุใช้เงินด่วน/ฉุกเฉิน พร้อมคืนสิ้นเดือนเมื่อได้เงินเดือน</li>
-                    </ul>
+                <div className="row">
+                    <div className="col-md-12">
+                        <ul>
+                            <li>สัญชาติไทย อายุ 21 ปีขึ้นไป</li>
+                            <li>ทำงานอยู่ในกทม.และปริมณฑล</li>
+                            <li>มีรายได้ต่อเดือนตั้งแต่ 15,000 บาทขึ้นไป และมีอายุงานตั้งแต่ 4 เดือนขึ้นไป</li>
+                            <li>มีเหตุใช้เงินด่วน/ฉุกเฉิน พร้อมคืนสิ้นเดือนเมื่อได้เงินเดือน</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <h4>ก่อนสมัครเตรียม 4 อย่างนี้ให้พร้อม</h4>
+                <div className="row">
+                    <div className="col-md-12">
+                        <h4>ก่อนสมัครเตรียม 4 อย่างนี้ให้พร้อม</h4>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <ol>
-                        <li>บัตรประชาชน</li>
-                        <li>สลิปเงินเดือน</li>
-                        <li>หลักฐานสำหรับวัตถุประสงค์ในการขอสินเชื่อบัญชีธนาคารที่เป็นบัญชีเงินเดือนย้อนหลัง
-                            3 เดือน</li>
-                        <li>บัญชีธนาคารที่เป็นบัญชีเงินเดือนย้อนหลัง 3 เดือน</li>
-                    </ol>
+                <div className="row">
+                    <div className="col-md-12">
+                        <ol>
+                            <li>บัตรประชาชน</li>
+                            <li>สลิปเงินเดือน</li>
+                            <li>หลักฐานสำหรับวัตถุประสงค์ในการขอสินเชื่อบัญชีธนาคารที่เป็นบัญชีเงินเดือนย้อนหลัง
+                                3 เดือน</li>
+                            <li>บัญชีธนาคารที่เป็นบัญชีเงินเดือนย้อนหลัง 3 เดือน</li>
+                        </ol>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <Link to="/register">
-                        <span>ใบสมัคร</span>
-                    </Link>
+                <div className="row">
+                    <div className="col-md-12">
+                        <Link to="/register">
+                            <span>ใบสมัคร</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
